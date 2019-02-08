21c375addff97ff93e3bde62218dc1bbff91cfeb
sportview <kamau63@gmail.com>
2019-02-08 13:16:24 +0300

    [finishes #163701382] added get all parties endpoint

--- myapp/app.py
+++ myapp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask,jsonify,make_response,request
 app=Flask(__name__)
 
-from myapp.api.v1.views.party import partyblue
+from myapp.api.v1.views.party.party import partyblue
 #from api.v1.views.Office.office import officeblue
 app.register_blueprint(partyblue,url_prefix="/api/v1")
 #app.register_blueprint(officeblue,url_prefix="/api/v1")
